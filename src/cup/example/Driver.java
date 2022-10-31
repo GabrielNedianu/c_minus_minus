327926d265fa2f5c714314afd837ce4f851dbad0
Nedianu Gabriel <57480858+GabrielNedianu@users.noreply.github.com>
2022-10-31 19:37:10 +0200

    Created a driver that identifies every token

--- src/cup/example/Driver.java
+++ src/cup/example/Driver.java
@@ -1,10 +1,30 @@
 package cup.example;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import java_cup.runtime.ComplexSymbolFactory;
+
 class Driver {
 
 	public static void main(String[] args) throws Exception {
-		Parser parser = new Parser();
-		parser.parse();
+		//	Parser parser = new Parser();
+		//	parser.parse();
+		ComplexSymbolFactory symbolFactory = new ComplexSymbolFactory();
+		File file = new File("input.txt");
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+		Lexer lexer = new Lexer(symbolFactory,fis);
+
+		int currentSymbolNr;
+		while ((currentSymbolNr=lexer.next_token().sym)!= sym.EOF )
+		{
+			System.out.println(currentSymbolNr);
+		}
 	}
-	
 }
